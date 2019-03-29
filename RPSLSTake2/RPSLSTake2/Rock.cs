feaf08310fdd723956bc0ae32fd2f5a7752afc9a
unknown <alex.point.maverick@icloud.com>
2019-03-29 15:20:14 -0500

    add rock class

---
+++ RPSLSTake2/RPSLSTake2/Rock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLSTake2
+{
+    class Rock : Gesture
+    {
+        // Member Variables
+        bool beatsPaper;
+        bool beatsScissors;
+        bool beatsLizard;
+        bool beatsSpock;
+        // Constructor
+        public Rock()
+        {
+            beatsPaper = false;
+            beatsScissors = true;
+        }
+        // Methods
+    }
+}
